73b2dabb2ccac05c39c2f98208eecf6bc0637102
Patrik Segedy <psegedy@redhat.com>
2025-08-20 10:13:30 +0200

    feat: cache price

--- src/tankarta_mcp/__init__.py
+++ src/tankarta_mcp/__init__.py
@@ -1,25 +1,40 @@
 import os
+import time
+from functools import lru_cache
+
 import httpx
 from fastmcp import FastMCP
 
-
+TTL = 6 * 60 * 60  # 6 hours in seconds
 TANKARTA_URL = os.getenv("TANKARTA_URL")
 mcp = FastMCP(
     "🚀 Orlen Tankarta ⛽",
     instructions="""
     This MCP server provides tools to get current discounted Orlen Tankarta Business price of fuel.
-    """
+    """,
 )
+
+
+@lru_cache(maxsize=None)
+def cached_price() -> tuple[str, float]:
+    """Get current discounted price of fuel."""
+    response = httpx.get(TANKARTA_URL)
+    return response.content.decode("utf-8"), time.time()
 
 
 @mcp.tool
 def price() -> str:
     """Get current discounted price of fuel."""
-    response = httpx.get(TANKARTA_URL)
-    return response.content.decode("utf-8")
+    price, ts = cached_price()
+    if time.time() - ts > TTL:
+        cached_price.cache_clear()
+        price, _ = cached_price()
+    return price
+
 
 def main() -> None:
     mcp.run()
+
 
 if __name__ == "__main__":
     main()
